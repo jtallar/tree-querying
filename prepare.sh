d91cc2d5ac461e14c1548daf33e27168abac1414
Julian Tallar <jtallar@itba.edu.ar>
2020-10-23 15:18:12 -0300

    First commit

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mvn clean install || { echo 'mvn clean install failed' ; exit 1 ; }
+cd server/target
+tar -xzf tpe2-g6-server-1.0-SNAPSHOT-bin.tar.gz
+chmod u+x tpe2-g6-server-1.0-SNAPSHOT/run-*
+cd ../..
+cd client/target
+tar -xzf tpe2-g6-client-1.0-SNAPSHOT-bin.tar.gz
+chmod u+x tpe2-g6-client-1.0-SNAPSHOT/run-*
+cd ../..
