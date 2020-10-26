fffda20581167812d99eb12d63ad610b59e1d042
Matias Brula <mbrula@itba.edu.ar>
2020-10-26 12:53:28 -0300

    query 1 - fix formayying 2

--- api/src/main/java/ar/edu/itba/pod/tpe/collators/Query1Collator.java
+++ api/src/main/java/ar/edu/itba/pod/tpe/collators/Query1Collator.java
@@ -4,10 +4,12 @@ import ar.edu.itba.pod.tpe.models.Neighbourhood;
 import ar.edu.itba.pod.tpe.utils.ComparablePair;
 import com.hazelcast.mapreduce.Collator;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 
 public class Query1Collator implements Collator<Map.Entry<String, Long>, Set<ComparablePair<Double, String>>> {
-
+    private static final int PLACES = 2;
     private final Map<String, Long> neighborhoods;
 
     public Query1Collator(Map<String, Long> neighborhoods) {
@@ -18,7 +20,13 @@ public class Query1Collator implements Collator<Map.Entry<String, Long>, Set<Com
     public Set<ComparablePair<Double, String>> collate(Iterable<Map.Entry<String, Long>> iterable) {
 
         Set<ComparablePair<Double, String>> out = new TreeSet<>(ComparablePair::compareToModified);
-        iterable.forEach(e -> out.add(new ComparablePair<>((double) e.getValue() / neighborhoods.get(e.getKey()), e.getKey())));
+        iterable.forEach(e -> out.add(new ComparablePair<>(round((double) e.getValue() / neighborhoods.get(e.getKey())), e.getKey())));
         return out;
+    }
+
+    private static double round(double value) {
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(PLACES, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
