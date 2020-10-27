c5f0b4c340d165165fbf4355a06ca60125e50b97
Matias Brula <mbrula@itba.edu.ar>
2020-10-27 11:23:44 -0300

    comment and normalize models

--- api/src/main/java/ar/edu/itba/pod/tpe/models/Tree.java
+++ api/src/main/java/ar/edu/itba/pod/tpe/models/Tree.java
@@ -5,51 +5,104 @@ import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.DataSerializable;
 
 import java.io.IOException;
+import java.util.Objects;
 
+/**
+ * Serializable Data class modeling the trees information.
+ */
 public class Tree implements DataSerializable {
-    private String street_name;
-    private String common_name;
+
+    private String streetName;
+    private String commonName;
     private double diameter;
 
-    // Empty constructor for Hazelcast
-    public Tree() {
+    /** Constructors */
 
+    public Tree() {
+        /** Empty constructor for Hazelcast */
     }
 
-    public Tree(String street_name, String common_name, double diameter) {
-        this.street_name = street_name;
-        this.common_name = common_name;
+    public Tree(String streetName, String commonName, double diameter) {
+        this.streetName = streetName;
+        this.commonName = commonName;
         this.diameter = diameter;
     }
 
-    public String getStreet_name() {
-        return street_name;
+    /** Getters */
+
+    public String getStreetName() {
+        return streetName;
     }
 
-    public String getCommon_name() {
-        return common_name;
+    public String getCommonName() {
+        return commonName;
     }
 
     public double getDiameter() {
         return diameter;
     }
 
-    @Override
-    public String toString() {
-        return street_name + "--" + common_name + "--" +  diameter;
-    }
-
+    /**
+     * Writes the data from a tree into a {@link ObjectDataOutput}
+     * @param out the object data to be saved serialized into
+     * @throws IOException when there is an error parsing the fields
+     */
     @Override
     public void writeData(ObjectDataOutput out) throws IOException {
-        out.writeUTF(street_name);
-        out.writeUTF(common_name);
+        out.writeUTF(streetName);
+        out.writeUTF(commonName);
         out.writeDouble(diameter);
     }
 
+    /**
+     * Reads the data from a {@link ObjectDataInput} and converts to a tree
+     * @param in the object data to be deserialized
+     * @throws IOException when there is an error parsing the fields
+     */
     @Override
     public void readData(ObjectDataInput in) throws IOException {
-        street_name = in.readUTF();
-        common_name = in.readUTF();
+        streetName = in.readUTF();
+        commonName = in.readUTF();
         diameter = in.readDouble();
+    }
+
+    /**
+     * Checks the two objects for equality by delegating to their respective
+     * {@link Object#equals(Object)} methods.
+     * @param o the {@link Tree} to which this one is to be checked for equality
+     * @return true if the underlying objects of the Tree are both considered equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tree)) return false;
+        Tree tree = (Tree) o;
+        return Double.compare(tree.diameter, diameter) == 0 &&
+                streetName.equals(tree.streetName) &&
+                commonName.equals(tree.commonName);
+    }
+
+    /**
+     * Compute a hash code using the hash codes of the underlying objects
+     * @return a hashcode of the Tree
+     */
+    @Override
+    public int hashCode() {
+        return (streetName == null ? 0 : streetName.hashCode()) ^
+                (commonName == null ? 0: commonName.hashCode()) ^
+                Double.hashCode(diameter);
+    }
+
+    /**
+     * Simply overrides the string printing for this class
+     * @return the resulting print string
+     */
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "streetName='" + streetName + '\'' +
+                ", commonName='" + commonName + '\'' +
+                ", diameter=" + diameter +
+                '}';
     }
 }
